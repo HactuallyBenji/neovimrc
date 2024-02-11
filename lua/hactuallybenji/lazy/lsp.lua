f46e96742ff0a0acb0c0b773f631eac0ce503e2f
Hactually Benji <mrbenamiller@gmail.com>
2024-02-11 13:48:13 -0800

    Need lsp languages, and remove borrowed remaps

--- lua/hactuallybenji/lazy/lsp.lua
+++ lua/hactuallybenji/lazy/lsp.lua
@@ -29,6 +29,7 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "tsserver",
+                "jdtls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
--- lua/hactuallybenji/lazy/treesitter.lua
+++ lua/hactuallybenji/lazy/treesitter.lua
@@ -6,7 +6,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "python", "json", "yaml", "html", "css", "scss"
+                "jsdoc", "bash", "python", "json", "yaml", "html", "css", "scss", "java",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
